59d3fefda0cd490d9871daf8944b2e61d68fb192
ValeriyBushenev <ValeriyBushenev@d8011a42-0409-11de-ada4-5153b8187bf2>
2009-05-09 18:51:39 +0000

    -
    git-svn-id: https://ontobus.googlecode.com/svn/trunk@29 d8011a42-0409-11de-ada4-5153b8187bf2

--- triple-storage/fast-read-triple-storage/build_with_dmd.sh
+++
@@ -1,4 +0,0 @@
-#del myDirectMap
-#/home/itiu/workplace/tango-tangobos-0.99.7-bin-linux-dmd.1.033/bin/dmd test_triple_storage.d HashMap.d TripleStorage.d Triple.d List.d Hash.d HashNeighbour.d KeysValueEntry.d -O -ofmytest_triple_storage
-dmd test_triple_storage.d HashMap.d TripleStorage.d Triple.d ListTriple.d Hash.d HashNeighbour.d KeysValueEntry.d -O -ofmytest_triple_storage
-dmd socketserver.d ListStrings.d HashMap.d TripleStorage.d Triple.d ListTriple.d Hash.d HashNeighbour.d KeysValueEntry.d -O -oftriple_storage_server
