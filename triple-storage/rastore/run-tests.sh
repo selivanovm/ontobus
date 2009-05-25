bb10979494ed24e9488117860c3117b78254d47a
ValeriyBushenev <ValeriyBushenev@d8011a42-0409-11de-ada4-5153b8187bf2>
2009-05-25 16:56:38 +0000

    упорядочивание структуры проекта
    git-svn-id: https://ontobus.googlecode.com/svn/trunk@51 d8011a42-0409-11de-ada4-5153b8187bf2

--- triple-storage/rastore/run-tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp /home/selivanovm/bin/scala-2.7.3.final/lib/scala-library.jar:./target/rastore-0.0.1-SNAPSHOT.jar:/home/selivanovm/soft/javalibs/*:/home/selivanovm/soft/javalibs/h2-1.0.67.jar ru.magnetosoft.rastore.TestRunner $1 $2
