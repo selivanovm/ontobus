35ea6a3acb70bf04cfa1ebc69486ace33915bdbd
ValeriyBushenev@gmail.com <ValeriyBushenev@gmail.com@d8011a42-0409-11de-ada4-5153b8187bf2>
2009-08-24 11:33:23 +0000

    cosmetic
    git-svn-id: https://ontobus.googlecode.com/svn/trunk@106 d8011a42-0409-11de-ada4-5153b8187bf2

--- agents/magnetico-authorization/semargl/src/authorization.d
+++ agents/magnetico-authorization/semargl/src/authorization.d
@@ -24,6 +24,8 @@ import tango.text.convert.Layout;
 import RightTypeDef;
 import script_util;
 
+import persistent_triple_storage;
+
 import S05InDocFlow;
 import S01AllLoggedUsersCanCreateDocuments;
 import S01UserIsAdmin;
@@ -97,7 +99,7 @@ class Authorization
 		foreach(file; scan.files)
 		{
 			Stdout.format("{}\n", file);
-			load_from_file(file);
+			load_from_file(file, i_know_predicates, ts);
 		}
 		Stdout.formatln("\n{} Errors", scan.errors.length);
 		foreach(error; scan.errors)
@@ -112,7 +114,7 @@ class Authorization
 		foreach(file; scan.files)
 		{
 			Stdout.format("{}\n", file);
-			load_from_file(file);
+			load_from_file(file, i_know_predicates, ts);
 		}
 		Stdout.formatln("\n{} Errors", scan.errors.length);
 		foreach(error; scan.errors)
@@ -121,106 +123,6 @@ class Authorization
 		Stdout.format("authorization init ... ok").newline;
 	}
 
-	private void load_from_file(FilePath file_path)
-	{
-		uint count_add_triple = 0;
-		uint count_ignored_triple = 0;
-
-		auto elapsed = new StopWatch();
-		double time;
-		Stdout.format("load triples from file {}", file_path).newline;
-
-		auto file = new File(file_path.path ~ file_path.name ~ file_path.suffix);
-		auto content = cast(char[]) file.read;
-
-		elapsed.start;
-
-		foreach(line; Text.lines(content))
-		{
-			char[] s, p, o;
-			int idx = 0;
-			foreach(element; Text.delimit(line, ">"))
-			{
-				element = Text.chopl(element, "<");
-				element = Text.chopl(element, " <");
-				element = Text.chopr(element, " .");
-				element = Text.trim(element);
-
-				if(element[4] == '-' && element[7] == '-' && element[10] == ' ' && element[13] == ':' && element[16] == ':' && element[19] == ',')
-					element = Text.delimit(element, "<")[1];
-
-				element[element.length] = 0;
-
-				idx++;
-				if(idx == 1)
-				{
-					s = element;
-				}
-
-				if(idx == 2)
-				{
-					p = element;
-				}
-
-				if(idx == 3)
-				{
-					if(element.length > 2)
-					{
-						o = element[1 .. (element.length - 1)];
-						o[o.length] = 0;
-					}
-				}
-
-			//				Stdout.format("element={} ", element).newline;
-
-			}
-
-			//			Stdout.format("main: add triple [{}] <{}><{}><{}>", count_add_triple, s, p, o).newline;
-
-			if(s.length == 0 && p.length == 0 && o.length == 0)
-				continue;
-
-			if(o.length == 2)
-			{
-				// Stdout.format("main: skip this triple [{}] <{}><{}><{}>", count_add_triple, s, p, o).newline;
-				continue;
-			}
-
-			bool i_know_predicat = false;
-			for(int i = 0; i < i_know_predicates.length; i++)
-			{
-				if(p == i_know_predicates[i])
-				{
-					i_know_predicat = true;
-					break;
-				}
-
-			}
-
-			if(i_know_predicat)
-			{
-				//						Stdout.format("main: add triple [{}] <{}><{}><{}>", count_add_triple, s, p, o).newline;
-				if(ts.addTriple(s, p, o))
-					count_add_triple++;
-				else
-					count_ignored_triple++;
-			}
-			else
-			{
-				count_ignored_triple++;
-
-			}
-		//				if(count_add_triple > 5)
-		//					break;
-		}
-
-		//	
-
-		time = elapsed.stop;
-
-		Stdout.format("create TripleStorage time = {}, count add triples = {}, ignored = {}", time, count_add_triple,
-				count_ignored_triple).newline;
-	}
 
 	public void addAuthorizeData(char[] s, char[] p, char[] o)
 	{
---
+++ agents/magnetico-authorization/semargl/src/persistent_triple_storage.d
@@ -0,0 +1,109 @@
+module persistent_triple_storage;
+
+import tango.io.File;
+import tango.io.FileScan;
+import tango.time.StopWatch;
+private import tango.io.Stdout;
+import Text = tango.text.Util;
+import TripleStorage;
+
+public void load_from_file(FilePath file_path, char[][] i_know_predicates, TripleStorage ts)
+{
+	uint count_add_triple = 0;
+	uint count_ignored_triple = 0;
+
+	auto elapsed = new StopWatch();
+	double time;
+	Stdout.format("load triples from file {}", file_path).newline;
+
+	auto file = new File(file_path.path ~ file_path.name ~ file_path.suffix);
+	auto content = cast(char[]) file.read;
+
+	elapsed.start;
+
+	foreach(line; Text.lines(content))
+	{
+		char[] s, p, o;
+		int idx = 0;
+		foreach(element; Text.delimit(line, ">"))
+		{
+			element = Text.chopl(element, "<");
+			element = Text.chopl(element, " <");
+			element = Text.chopr(element, " .");
+			element = Text.trim(element);
+
+			if(element[4] == '-' && element[7] == '-' && element[10] == ' ' && element[13] == ':' && element[16] == ':' && element[19] == ',')
+				element = Text.delimit(element, "<")[1];
+
+			element[element.length] = 0;
+
+			idx++;
+			if(idx == 1)
+			{
+				s = element;
+			}
+
+			if(idx == 2)
+			{
+				p = element;
+			}
+
+			if(idx == 3)
+			{
+				if(element.length > 2)
+				{
+					o = element[1 .. (element.length - 1)];
+					o[o.length] = 0;
+				}
+			}
+
+		//				Stdout.format("element={} ", element).newline;
+
+		}
+
+		//			Stdout.format("main: add triple [{}] <{}><{}><{}>", count_add_triple, s, p, o).newline;
+
+		if(s.length == 0 && p.length == 0 && o.length == 0)
+			continue;
+
+		if(o.length == 2)
+		{
+			// Stdout.format("main: skip this triple [{}] <{}><{}><{}>", count_add_triple, s, p, o).newline;
+			continue;
+		}
+
+		bool i_know_predicat = false;
+		for(int i = 0; i < i_know_predicates.length; i++)
+		{
+			if(p == i_know_predicates[i])
+			{
+				i_know_predicat = true;
+				break;
+			}
+
+		}
+
+		if(i_know_predicat)
+		{
+			//						Stdout.format("main: add triple [{}] <{}><{}><{}>", count_add_triple, s, p, o).newline;
+			if(ts.addTriple(s, p, o))
+				count_add_triple++;
+			else
+				count_ignored_triple++;
+		}
+		else
+		{
+			count_ignored_triple++;
+
+		}
+	//				if(count_add_triple > 5)
+	//					break;
+	}
+
+	//	
+
+	time = elapsed.stop;
+
+	Stdout.format("create TripleStorage time = {}, count add triples = {}, ignored = {}", time, count_add_triple,
+			count_ignored_triple).newline;
+}
