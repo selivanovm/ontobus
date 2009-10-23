0ffe4a3540bb435c743ca639820b95a2f8b56ee9
Your Name <jeer4bear@gmail.com>
2009-10-23 17:05:07 +0400

    is_in_docflow modifications

--- agents/magnetico-authorization/semargl/src/scripts/S05InDocFlow.d
+++ agents/magnetico-authorization/semargl/src/scripts/S05InDocFlow.d
@@ -13,21 +13,34 @@ public int calculate(char* user, char* elementId, uint rightType, TripleStorage 
 	// Если документ хотят редактировать или удалять
 	// Но он находится в документообороте
 	char* df_right = isInDocFlow(elementId, ts);
+	log.trace("isInDocFlow : subject = {}", fromStringz(df_right));
 	if(df_right !is null)
 	{
+
+	  log.trace("isInDocFlow #1"):
 
 		uint* iterator = ts.getTriples(df_right, "magnet-ontology/authorization/acl#targetSubsystemElement", user);
 		if(iterator !is null)
 		{
+
+		log.trace("isInDocFlow #2"):
+
 			iterator = ts.getTriples(df_right, "magnet-ontology/authorization/acl#rights", null);
 			if(iterator !is null)
 			{
+
+			log.trace("isInDocFlow #3"):
+
 				byte* triple = cast(byte*) *iterator;
 				if(triple !is null)
 				{
+
+				log.trace("isInDocFlow #4"):
+
 					// проверим, есть ли тут требуемуе нами право
 					char* triple2_o = cast(char*) (triple + 6 + (*(triple + 0) << 8) 
 								       + *(triple + 1) + 1 + (*(triple + 2) << 8) + *(triple + 3) + 1);
+
 
 					while(*triple2_o != 0)
 					{
