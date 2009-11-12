6c90e11ade2278ccba326f40c421034d2c013ddc
itiu <itiu@itiu-desktop.(none)>
2009-11-12 16:24:49 +0300

    autotest++

--- agents/magnetico-authorization/semargl/src/autotest.d
+++ agents/magnetico-authorization/semargl/src/autotest.d
@@ -1,7 +1,9 @@
 private import mom_client;
 private import Log;
 private import tango.io.File;
-private import Text = tango.text.Util;
+//private import Text = tango.text.Util;
+private import tango.text.Text;
+//private import tango.text.Regex;
 private import portions_read;
 private import tango.stdc.string;
 private import tango.stdc.stringz;
@@ -29,7 +31,7 @@ class autotest: mom_client
 
 	int send(char* routingkey, char* messagebody)
 	{
-		printf("\nOUTPUT: %s\n", output_data);
+		//		printf("\nOUTPUT: %s\n", output_data);
 
 		if(strcmp(messagebody, output_data) != 0)
 		{
@@ -79,9 +81,43 @@ void prepare_block(char* line, ulong line_length)
 		*end_input_block = 0;
 
 	int size = end_input_block - input_data;
-	printf("\nINPUT %d: %s\n", size, input_data);
+	//	printf("\nINPUT %d: %s\n", size, input_data);
 
-	printf("\nOUTPUT: %s\n", output_data);
+	//	printf("\nOUTPUT: %s\n", output_data);
+
+	if(strstr(input_data, "magnet-ontology/authorization/functions#create") !is null && strstr(input_data, "<>") !is null)
+	{
+		printf("\nINPUT %d: %s\n", size, input_data);
+
+		printf("\nOUTPUT: %s\n", output_data);
+
+		// это команда на создание записи авторизации
+
+		char[] result_id_tag = "<magnet-ontology/transport#result:data>";
+
+		char* result_id = strstr(output_data, result_id_tag.ptr);
+		if(result_id !is null)
+			result_id += result_id_tag.length;
+
+		int size_id = strlen(result_id) - 2;
+		char[] result_id_text = result_id[1 .. size_id];
+		char[] qqq = input_data[0 .. size];
+
+		//		auto rr = Regex(result_id_text).replaceAll ("<>", "<" ~ result_id_text ~ ">");
+		//		printf("\nresult: %s\n", rr.ptr);
+
+		auto input_data_text = new Text!(char)(qqq);
+		while(strstr(input_data_text.toString().ptr, "<>") !is null)
+		{
+			input_data_text.select("<>");
+			input_data_text.replace("<" ~ result_id_text ~ ">");
+		}
+
+		input_data = toStringz(input_data_text.toString());
+		size = strlen(input_data);
+
+		printf("\nresult: %s\n", input_data);
+	}
 
 	message_acceptor(cast(byte*) input_data, size);
 }
