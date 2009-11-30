3be275a01ec6beaec0955c302f2fd7e235c4f87f
selivanovm <selivanovm@selivanovm-desktop.(none)>
2009-11-30 16:19:12 +0300

    new authorization script S09DocumentOfTemplate

---
+++ agents/magnetico-authorization/semargl/src/category.d
@@ -0,0 +1,10 @@
+module Category;
+
+public static const char[] DOCUMENT = "DOCUMENT";
+public static const char[] DOCUMENT_DRAFT = "DOCUMENTTDRAFT";
+public static const char[] DOCUMENT_TEMPLATE = "DOCUMENTTEMPLATE";
+public static const char[] DOCUMENT_TEMPLATE_DRAFT = "DOCUMENTTEMPLATEDRAFT";
+
+public static const char[] PERMISSION = "PERMISSION";
+
+public static const char[] DOCUMENTS_OF_TEMPLATE = "DOCUMENTS_OF_TEMPLATE";
