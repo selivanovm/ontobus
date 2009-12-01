a64894189decf28a4a51e885b1bbb837b2f4e7c5
selivanovm <selivanovm@selivanovm-desktop.(none)>
2009-12-01 10:53:31 +0300

    changed: added more testing functionality

--- agents/triple-storages/rastore/scripts/Client.scala
+++ agents/triple-storages/rastore/scripts/Client.scala
@@ -7,9 +7,9 @@ object Client {
 
   def main(args: Array[String]) {
     
-    if (args.size == 12) {
+    if (args.size == 15) {
 
-      val message = args(0)
+      val msg = args(0)
       val hostName = args(1)
       val portNumber = args(2)
       val virtualHost = args(3)
@@ -21,10 +21,14 @@ object Client {
       val routingKey = args(9)
       val exchangeType = args(10)
       val listenAfterSending = args(11)
+      val msgCount = if (args(12) == null) 1 else args(12).toInt
+      val msgMultiplier = if (args(13) == null) 1 else args(13).toInt
+      val listen = if (args(14) == null || args(14) != "true") false else true
 
       println(String.format("host = %s, port = %s, virtualHost = %s, exchange = %s, queue = %s, userName = %s, password = %s, heartBeat = %s, " +
-                            "routingKey = %s, exchangeType = %s, listenAfterSending = %s, message = %s", hostName, portNumber, virtualHost,
-                          exchange, queue, userName, password, heartBeat, routingKey, exchangeType, listenAfterSending, message))
+                            "routingKey = %s, exchangeType = %s, listenAfterSending = %s, message = %s, count = %s, multiplier = %s listen = %s", 
+                            hostName, portNumber, virtualHost, exchange, queue, userName, password, heartBeat, routingKey, exchangeType, listenAfterSending, 
+                            msg, int2Integer(msgCount), int2Integer(msgMultiplier), boolean2Boolean(listen)))
 
       var params = new ConnectionParameters()
       params.setUsername(userName)
@@ -36,32 +40,95 @@ object Client {
       val conn = factory.newConnection(hostName, portNumber.toInt)
       
       val channel = conn.createChannel()
-      
-//      channel.exchangeDeclare(exchange, exchangeType)
-      if (routingKey.length == 0) {
-        channel.queueDeclare(exchange)
-      }
-      channel.queueDeclare(queue)
-//      channel.queueBind(queue, exchange, routingKey)
 
-      channel.basicPublish(routingKey, exchange,
-                           MessageProperties.PERSISTENT_TEXT_PLAIN,
-                           message.getBytes)
+      //      channel.exchangeDeclare(exchange, exchangeType)
+      //      channel.queueBind(queue, exchange, routingKey)
 
-      if (listenAfterSending == "true") {
+      if (!listen) {
+
+        if (routingKey.length == 0) {
+          try {
+            channel.queueDelete(exchange)
+          } catch {
+            case ex: Exception =>
+          }
+          channel.queueDeclare(exchange, false)
+        }
+        try {
+          channel.queueDelete(queue)
+        } catch {
+          case ex: Exception =>
+        }
+        channel.queueDeclare(queue, false)
+
+        var messageBuilder = new StringBuilder(1000)
+        for(i <- 0 until msgMultiplier) {
+          messageBuilder.append(msg)
+        }
+        val message = messageBuilder.toString
+
+        val startTime = System.nanoTime;
+        for(i <- 0 until msgCount) {
+          channel.basicPublish(routingKey, exchange,
+                               MessageProperties.TEXT_PLAIN,
+                               message.getBytes)
+
+          if (listenAfterSending == "true") {
+            val consumer = new QueueingConsumer(channel)
+            channel.basicConsume(queue, true, consumer)
+
+            while(true) {
+              val delivery = consumer.nextDelivery
+              val body = new String(delivery.getBody)
+              println("Got answer : " + body)
+            }
+          }
+        }
+
+        val execTime = (System.nanoTime - startTime) / 1000000000.0
+        val sentBytesCount = message.length * msgCount
+        println(String.format("Message size %s", int2Integer(message.length)))
+        println(String.format("Total messages sent: %s", int2Integer(msgCount)))
+        println(String.format("Total bytes sent: %s", int2Integer(sentBytesCount)))
+        println(String.format("In %s sec.", double2Double(execTime)))
+        println(String.format("Speed %.2f bytes/sec", double2Double(sentBytesCount / execTime)))
+        println(String.format("Speed %.2f msgs/sec", double2Double(msgCount / execTime)))
+
+      } else {
+
+        if (routingKey.length == 0) {
+          channel.queueDeclare(exchange, false)
+        }
+        channel.queueDeclare(queue, false)
+
         val consumer = new QueueingConsumer(channel)
         channel.basicConsume(queue, true, consumer)
 
-        while(true) {
+        var receivedBytesCount : Long = 0
+        var receptionTime : Long= 0
+        var receivedMsgCount : Long = 0
+        var i = msgCount
+        while(i > 0) {
+          val start = System.nanoTime
+
           val delivery = consumer.nextDelivery
+
+          val end = System.nanoTime
           val body = new String(delivery.getBody)
-          println("Got answer : " + body)
+
+          receivedBytesCount += body.length
+          receptionTime += end - start;
+          receivedBytesCount += 1
+          i -= 1
         }
+
+        println(String.format("Receiving Speed %.2f bytes/sec", double2Double(receivedBytesCount / (receptionTime / 1000000000.0))))
+        println(String.format("Receiving Speed %.2f msgs/sec", double2Double(msgCount / (receptionTime / 1000000000.0))))
 
       }
 
     } else
-      println ("Usage : <message> <host> <port> <vhost> <exchange> <targetQueue> <user> <password> <heartbeat> <routingKey> <exchangeType> <listen_flag>")
+      println ("Usage : <message> <host> <port> <vhost> <exchange> <targetQueue> <user> <password> <heartbeat> <routingKey> <exchangeType> <listenFlag>")
     println ("Done.")
     System.exit(0)
   }
