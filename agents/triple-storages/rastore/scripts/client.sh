5f9fa4d44d83e46559c0e2832d08c600bebe4764
selivanovm <selivanovm@selivanovm-desktop.(none)>
2009-12-01 10:54:37 +0300

    added: testing script

---
+++ agents/triple-storages/rastore/scripts/client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#sudo rabbitmqctl add_user rastore rspass
+#sudo rabbitmqctl add_vhost auth
+#sudo rabbitmqctl map_user_vhost rastore auth
+
+scalac -cp /home/selivanovm/soft/javalibs/rabbitmq-client.jar Client.scala
+/usr/lib/jvm/java-1.5.0-sun/bin/java -cp /home/selivanovm/soft/javalibs/commons-io-1.2.jar:/home/selivanovm/soft/javalibs/commons-cli-1.1.jar:/home/selivanovm/soft/javalibs/rabbitmq-client.jar:./:/home/selivanovm/bin/scala-2.7.3.final/lib/scala-library.jar Client "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "$12" "$13" "$14" "$15"
