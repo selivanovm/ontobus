9a7e4b5af1529dcd3671dcf817e487ea74dd2624
selivanovm <selivanovm@selivanovm-desktop.(none)>
2009-12-14 14:53:58 +0300

    ololo

--- agents/triple-storages/rastore/scripts/client.sh
+++ agents/triple-storages/rastore/scripts/client.sh
@@ -3,5 +3,5 @@
 #sudo rabbitmqctl add_vhost auth
 #sudo rabbitmqctl map_user_vhost rastore auth
 
-scalac -cp /home/selivanovm/soft/javalibs/rabbitmq-client.jar Client.scala
+#scalac -cp /home/selivanovm/soft/javalibs/rabbitmq-client.jar Client.scala
 /usr/lib/jvm/java-6-sun/bin/java -cp /home/selivanovm/soft/javalibs/commons-io-1.2.jar:/home/selivanovm/soft/javalibs/commons-cli-1.1.jar:/home/selivanovm/soft/javalibs/rabbitmq-client.jar:./:/home/selivanovm/bin/scala-2.7.3.final/lib/scala-library.jar Client "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "$12" "$13" "$14" "$15"
