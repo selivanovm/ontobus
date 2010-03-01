dcd78d56e6cc0df8b95a29ff60d8488852358b97
itiu <itiu@itiu-desktop.(none)>
2010-03-01 10:36:47 +0300

    -

--- agents/magnetico-authorization/semargl/build_with_dmd.bat
+++ agents/magnetico-authorization/semargl/build_with_dmd.bat
@@ -3,7 +3,11 @@ del *.test
 del *.agent
 del *.exe
 
+<<<<<<< HEAD:agents/magnetico-authorization/semargl/build_with_dmd.bat
+dmd -version=tango_99_8 -Iexport C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/net/InternetAddress C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/net/Socket C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/stdc/stdarg.d C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/stdc/errno.d -Iimport src\authorization.d src\autotest.d src\category.d src\fact_tools.d src\Log.d src\mom_client.d src\persistent_triple_storage.d src\portions_read.d src\RightTypeDef.d src\script_util.d src\server.d src\mod\tango\io\device\Conduit.d src\mod\tango\io\device\Device.d src\mod\tango\io\device\File.d src\scripts\S01AllLoggedUsersCanCreateDocuments.d src\scripts\S01UserIsAdmin.d src\scripts\S05InDocFlow.d src\scripts\S09DocumentOfTemplate.d src\scripts\S10UserIsAuthorOfDocument.d src\scripts\S10UserIsPermissionTargetAuthor.d src\scripts\S11ACLRightsHierarhical.d lib/librabbitmq_client.lib lib/librabbitmq.lib lib/TripleStorage.lib -O -release -ofSemargl.exe
+=======
 dmd -version=tango_99_8 -Iexport C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/net/InternetAddress C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/net/Socket C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/stdc/stdarg.d C:\tango-0.99.8-bin-win32-dmd.1.041\import/tango/stdc/errno.d -Iimport src\authorization.d src\autotest.d src\category.d src\fact_tools.d src\Log.d src\mom_client.d src\persistent_triple_storage.d src\portions_read.d src\RightTypeDef.d src\script_util.d src\server.d src\mod\tango\io\device\Conduit.d src\mod\tango\io\device\Device.d src\mod\tango\io\device\File.d src\scripts\S01AllLoggedUsersCanCreateDocuments.d src\scripts\S01UserIsAdmin.d src\scripts\S05InDocFlow.d src\scripts\S09DocumentOfTemplate.d src\scripts\S10UserIsAuthorOfDocument.d src\scripts\S10UserIsPermissionTargetAuthor.d src\scripts\S11ACLRightsHierarhical.d lib/mom_client.lib lib/librabbitmq_client.lib lib/librabbitmq.lib lib/TripleStorage.lib -O -release -ofSemargl.exe
+>>>>>>> rmqd:agents/magnetico-authorization/semargl/build_with_dmd.bat
 
 rem dmd src/Triple.d src/TripleStorage.d src/Log.d src/HashMap.d src/Hash.d src/librabbitmq_headers.d src/librabbitmq_listen.d src/server.d lib/librabbitmq.a -O -release -ofSemargl.agent
 
